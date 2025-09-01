0f2a7239606deaa8e29c382265b410006c22177c
Víctor Gómez <61844500+victorjgomez@users.noreply.github.com>
2025-08-31 20:17:22 -0400

    Inject MemberService via constructor

--- src/main/java/com/library/library/member/MemberResources.java
+++ src/main/java/com/library/library/member/MemberResources.java
@@ -1,14 +1,20 @@
 package com.library.library.member;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MemberResources {
 
+    private final MemberService memberService;
+
+    public MemberResources(MemberService memberService) {
+        this.memberService = memberService;
+    }
 
     @PostMapping("/members")
-    public Member createMember(Member member){
-        throw new UnsupportedOperationException();
+    public Member createMember(@RequestBody Member member) {
+        return memberService.createMember(member);
     }
 }
